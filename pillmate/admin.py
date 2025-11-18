4d16e48dc2b74ef7cccd68e5caa96df9fe488779
osein607 <osein607@naver.com>
2025-11-18 10:07:42 +0900

    ✨admin.py 세팅

--- pillmate/admin.py
+++ pillmate/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Medicine
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("기본 정보", {
+            "fields": ("user", "name", "type")
+        }),
+        ("복약 설정", {
+            "fields": ("quantity", "time", "alarm_time")
+        }),
+        ("복약 기간", {
+            "fields": ("start_date", "end_date")
+        }),
+        ("기타", {
+            "fields": ("is_taken_today",)
+        }),
+    )
